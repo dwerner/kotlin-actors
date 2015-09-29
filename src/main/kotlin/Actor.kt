b4f1bf626a4ae9ec9080642cd33d28adb12957e2
Daniel Werner <dan.werner@hubblehome.com>
2015-09-29 12:53:57 -0700

    Autodetect android and grab the main thread through looper

--- src/main/kotlin/Actor.kt
+++ src/main/kotlin/Actor.kt
@@ -1,14 +1,7 @@
 package com.hubble.actors
 
 import java.util.ArrayList
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.ThreadFactory
-
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.SettableFuture
 import com.google.common.util.concurrent.MoreExecutors
@@ -16,136 +9,168 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder
 import com.google.common.util.concurrent.ListeningScheduledExecutorService
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.FutureCallback
+import java.util.concurrent.*
+
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 
 object Aether {
-  private val threadFactory: ThreadFactory? = ThreadFactoryBuilder().setDaemon(true)?.build()
-  private val pool = Executors.newScheduledThreadPool(8, threadFactory as ThreadFactory)
-  internal val executor: ListeningScheduledExecutorService = MoreExecutors.listeningDecorator(pool)
-  // TODO: private val mainThreadExecutor = getPlatformMainThreadExecutor()
+	private val threadFactory: ThreadFactory? = ThreadFactoryBuilder().setDaemon(true)?.build()
+	private val pool = Executors.newScheduledThreadPool(8, threadFactory as ThreadFactory)
+	internal val executor: ListeningScheduledExecutorService = MoreExecutors.listeningDecorator(pool)
+	internal val mainThreadExecutor: Executor = getPlatformMainThreadExecutor()
+
+	private fun getPlatformMainThreadExecutor(): Executor {
+		return try {
+			Class.forName("android.os.Build");
+			object : Executor {
+				private val handler: Handler = Handler(Looper.getMainLooper())
+				override fun execute(r: Runnable) {
+					handler.post(r);
+				}
+			}
+		} catch (cnf: Exception) {
+			object : Executor {
+				override fun execute(command: Runnable) {
+					command.run()
+				}
+			}
+		}
+	}
 }
 
 public abstract class Actor() {
 
-  // Internal messages
-  private data class Message(val message: Any?, val future: SettableFuture<Any>)
-  private data class Death()
+	// Internal messages
+	private data class Message(val message: Any?, val future: SettableFuture<Any>)
 
-  // atomics
-  private val running = AtomicBoolean(false)
-  private val alive = AtomicBoolean(true)
-  private val mailbox: BlockingQueue<Message> = LinkedBlockingQueue<Message>()
+	private data class Death()
 
-  /**
-   * receive messages - implement this to define the actor
-   */
-  abstract fun receive(m: Any?): Any?
+	// atomics
+	private val running = AtomicBoolean(false)
+	private val alive = AtomicBoolean(true)
+	private val mailbox: BlockingQueue<Message> = LinkedBlockingQueue<Message>()
 
-  /**
-   * rescue from an exception
-   */
-  open fun rescue(error: ActorExecutionException) {
-    throw error
-  }
+	/*
+	 * Run a function on the main thread
+	 */
+	public fun runOnMainThread(fn: () -> Unit) {
+		Aether.mainThreadExecutor.execute { fn() }
+	}
 
-  /**
-   * send a message to this actor
-   */
-  fun send(m: Any?): ListenableFuture<Any> {
-    if (!this.alive.get()) {
-      throw ActorExecutionException("Cannot send messages to a dead actor.", m, Exception())
-    }
-    val result = promise<Any>()
-    mailbox.put(Message(m, result))
-    dispatch()
-    return result
-  }
+	public fun runOnMainThread(r: Runnable) = Aether.mainThreadExecutor.execute(r)
 
-  /**
-   * schedule a message to be sent to the actor's mailbox after a given delay
-   * Since this relies on the messagebox still, will not execute if the actor has been killed.
-   */
-  fun after(interval: Long, m: Any?): ListenableFuture<Any?> {
-    val afterPromise = promise<Any?>()
-    Aether.executor.schedule({() ->
-      try {
-        if (alive.get()) {
-          afterPromise.set((this send m).get())
-        }
-      }
-      catch (e: Exception) {
-        afterPromise.setException(
-            ActorExecutionException("Failed to execute scheduled message.", m, e)
-        )
-      }
-    }, interval, TimeUnit.MILLISECONDS)
-    return afterPromise
-  }
+	/**
+	 * receive messages - implement this to define the actor
+	 */
+	abstract fun receive(m: Any?): Any?
 
-  /***
-   * kill this actor; immediately stop processing new messages.
-   * Any currently executing promises will finish normally.
-   */
-  open fun kill() {
-    this.alive.set( false ) // immediately stop processing messages
-    this.mailbox.clear()
-  }
+	/**
+	 * rescue from an exception
+	 */
+	open fun rescue(error: ActorExecutionException) {
+		throw error
+	}
 
-  // Run our mailbox to completion, and then return and wait for more messages
-  private fun dispatch() {
-    if ( !running.get() && !mailbox.isEmpty() && alive.get() ) {
-      if ( running.compareAndSet(false, true) ) {
-        val messages = ArrayList<Message>()
-        mailbox.drainTo(messages, 8)
-        Aether.executor submit {() ->
-          for (msg in messages) {
-            try {
-              if ( alive.get() ) {
-                msg.future.set(receive(msg.message))
-              }
-            }
-            catch (t: Throwable) {
-              msg.future.setException(
-                  ActorExecutionException("Failed to execute message.", msg.message, t)
-              )
-            }
-          }
-          running.set(false)
-          dispatch()
-        }
-      }
-    }
-  }
-  /***
-   * creates a Listenable/Settable future representing an eventual value and adds
-   * a handler that captures exceptions and proxies them to the main thread.
-   * (This is needed, otherwise we eat Throwables.)
-   */
-  public fun <T> promise(): SettableFuture<T> {
-    val promise = SettableFuture.create<T>()
-    Futures.addCallback(promise, object : FutureCallback<T> {
-      override fun onFailure(t: Throwable?) {
-        rescue( t!! as ActorExecutionException )
-      }
-      override fun onSuccess(result: T) {}
-    })
-    return promise
-  }
+	/**
+	 * send a message to this actor
+	 */
+	fun send(m: Any?): ListenableFuture<Any> {
+		if (!this.alive.get()) {
+			throw ActorExecutionException("Cannot send messages to a dead actor.", m, Exception())
+		}
+		val result = promise<Any>()
+		mailbox.put(Message(m, result))
+		dispatch()
+		return result
+	}
 
+	/**
+	 * schedule a message to be sent to the actor's mailbox after a given delay
+	 * Since this relies on the messagebox still, will not execute if the actor has been killed.
+	 */
+	fun after(interval: Long, m: Any?): ListenableFuture<Any?> {
+		val afterPromise = promise<Any?>()
+		Aether.executor.schedule({
+			try {
+				if (alive.get()) {
+					afterPromise.set((this send m).get())
+				}
+			} catch (e: Exception) {
+				afterPromise.setException(
+						ActorExecutionException("Failed to execute scheduled message.", m, e)
+				)
+			}
+		}, interval, TimeUnit.MILLISECONDS)
+		return afterPromise
+	}
+
+	/***
+	 * kill this actor; immediately stop processing new messages.
+	 * Any currently executing promises will finish normally.
+	 */
+	open fun kill() {
+		this.alive.set(false) // immediately stop processing messages
+		this.mailbox.clear()
+	}
+
+	// Run our mailbox to completion, and then return and wait for more messages
+	private fun dispatch() {
+		if ( !running.get() && !mailbox.isEmpty() && alive.get() ) {
+			if ( running.compareAndSet(false, true) ) {
+				val messages = ArrayList<Message>()
+				mailbox.drainTo(messages, 8)
+				Aether.executor submit {
+					for (msg in messages) {
+						try {
+							if ( alive.get() ) {
+								msg.future.set(receive(msg.message))
+							}
+						} catch (t: Throwable) {
+							msg.future.setException(
+									ActorExecutionException("Failed to execute message.", msg.message, t)
+							)
+						}
+					}
+					running.set(false)
+					dispatch()
+				}
+			}
+		}
+	}
+
+	/***
+	 * creates a Listenable/Settable future representing an eventual value and adds
+	 * a handler that captures exceptions and proxies them to the main thread.
+	 * (This is needed, otherwise we eat Throwables.)
+	 */
+	public fun <T> promise(): SettableFuture<T> {
+		val promise = SettableFuture.create<T>()
+		Futures.addCallback(promise, object : FutureCallback<T> {
+			override fun onFailure(t: Throwable?) {
+				rescue(t!! as ActorExecutionException)
+			}
+
+			override fun onSuccess(result: T) {
+			}
+		})
+		return promise
+	}
 }
-
 
 public abstract class LinkedActor(val id: String, val link: Actor) : Actor() {
 
-  public data class LinkedDeath(val id: String)
+	public data class LinkedDeath(val id: String)
 
-  fun sendLink(m: Any?): Any? {
-    return link send m
-  }
+	fun sendLink(m: Any?): Any? {
+		return link send m
+	}
 
-  override fun kill() {
-    super<Actor>.kill()
-    link send LinkedDeath(id) // Actor{id} was killed
-  }
+	override fun kill() {
+		super<Actor>.kill()
+		link send LinkedDeath(id) // Actor{id} was killed
+	}
 
 }
 
@@ -153,37 +178,37 @@ public abstract class LinkedActor(val id: String, val link: Actor) : Actor() {
  * Actor extension - Publish/Subscribe capabilities
  */
 public abstract class PublishSubscribeActor() : Actor() {
-  private val clients = ArrayList<Actor>()
-  fun publish(m: Any?) {
-    for (a in clients) {
-      a send m
-    }
-  }
+	private val clients = ArrayList<Actor>()
+	fun publish(m: Any?) {
+		for (a in clients) {
+			a send m
+		}
+	}
 
-  fun subscribe(a: Actor) {
-    if (!clients.contains(a) ) {
-      clients.add(a)
-    }
-  }
+	fun subscribe(a: Actor) {
+		if (!clients.contains(a) ) {
+			clients.add(a)
+		}
+	}
 
-  fun unsubscribe(a: Actor) {
-    if ( clients.contains(a) ) {
-      clients.remove(a)
-    }
-  }
+	fun unsubscribe(a: Actor) {
+		if ( clients.contains(a) ) {
+			clients.remove(a)
+		}
+	}
 }
 
 public class ActorExecutionException(
-    msg: String,
-    val message: Any?,
-    throwable: Throwable
+		msg: String,
+		val message: Any?,
+		throwable: Throwable
 ) : Exception(msg, throwable)
 
 public fun actor(f: (Any?) -> Any?): Actor {
-  return object : Actor() {
-    override fun receive(m: Any?): Any? {
-      return f(m)
-    }
-  }
+	return object : Actor() {
+		override fun receive(m: Any?): Any? {
+			return f(m)
+		}
+	}
 }
 
--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -1,6 +1,7 @@
 /**
  * Created by dan on 01/10/14.
  */
+
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListenableFutureTask;
 import com.google.common.util.concurrent.ListenableScheduledFuture;
@@ -23,115 +24,121 @@ import jet.runtime.typeinfo.JetValueParameter;
 import static kotlin.io.IoPackage.println;
 
 public class JavaTest {
-  private class Message {
-    Actor a;
-    public Message(final Actor a){
-      this.a = a;
-    }
-    public Actor getA(){
-      return a;
-    }
-  }
+	private class Message {
+		Actor a;
 
-  private class PingMessage extends Message {
-    public PingMessage(Actor a) {
-      super(a);
-    }
-  }
+		public Message(final Actor a) {
+			this.a = a;
+		}
 
-  private class PongMessage extends Message {
-    public PongMessage(Actor a) {
-      super(a);
-    }
-  }
+		public Actor getA() {
+			return a;
+		}
+	}
 
-  private class Pong extends Actor {
-    AtomicInteger msgCount = new AtomicInteger(0);
-    @Override
-    public Object receive(Object m) {
-      if (m instanceof PingMessage) {
-        msgCount.getAndAdd(1);
-        //println("Ping from ThreadID" + Thread.currentThread().getId());
-        ((PingMessage)m).getA().send(new PongMessage(this));
-      }
-      return null;
-    }
-  }
+	private class PingMessage extends Message {
+		public PingMessage(Actor a) {
+			super(a);
+		}
+	}
 
-  private class Ping extends Actor {
-    AtomicInteger msgCount = new AtomicInteger(0);
-    @Override
-    public Object receive(Object m) {
-      if (m instanceof PongMessage) {
-        msgCount.getAndAdd(1);
-        ((PongMessage) m).getA().send(new PingMessage(this));
-      }
-      return null;
-    }
-  }
+	private class PongMessage extends Message {
+		public PongMessage(Actor a) {
+			super(a);
+		}
+	}
+
+	private class Pong extends Actor {
+		AtomicInteger msgCount = new AtomicInteger(0);
+
+		@Override
+		public Object receive(Object m) {
+			if (m instanceof PingMessage) {
+				msgCount.getAndAdd(1);
+				//println("Ping from ThreadID" + Thread.currentThread().getId());
+				((PingMessage) m).getA().send(new PongMessage(this));
+			}
+			return null;
+		}
+	}
+
+	private class Ping extends Actor {
+		AtomicInteger msgCount = new AtomicInteger(0);
+
+		@Override
+		public Object receive(Object m) {
+			if (m instanceof PongMessage) {
+				msgCount.getAndAdd(1);
+				((PongMessage) m).getA().send(new PingMessage(this));
+			}
+			return null;
+		}
+	}
 
 
-  @Test
-  public void ActorPingPongTest(){
-    long startTime = new Date().getTime();
-    println("Starting Ping/Ping test from ThreadID" + Thread.currentThread().getId());
-    List<ListenableFuture<?>> pings = new ArrayList<ListenableFuture<?>>();
-    Ping ping = new Ping();
-    Pong pong = new Pong(); // Don't reflect messages
-    for (int i = 0; i < 500000; i++) {
-      pings.add(ping.send(new PongMessage(pong)));
-    }
-    try {
-      for (ListenableFuture<?> p : pings) {
-        p.get();
-      }
-      long endTime = new Date().getTime();
-      println("Ending ActorPingPongTest with " + pings.size() + " sent messages in "+ (endTime - startTime) + " ms");
-      println("Total messages received:"+ ping.msgCount + " + " + pong.msgCount);
-    } catch (Exception e) {
-      Assert.fail("exception during ping-pong");
-    }
-  }
+	@Test
+	public void ActorPingPongTest() {
+		long startTime = new Date().getTime();
+		println("Starting Ping/Ping test from ThreadID" + Thread.currentThread().getId());
+		List<ListenableFuture<?>> pings = new ArrayList<ListenableFuture<?>>();
+		Ping ping = new Ping();
+		Pong pong = new Pong(); // Don't reflect messages
+		for (int i = 0; i < 500000; i++) {
+			pings.add(ping.send(new PongMessage(pong)));
+		}
+		try {
+			for (ListenableFuture<?> p : pings) {
+				p.get();
+			}
+			long endTime = new Date().getTime();
+			println("Ending ActorPingPongTest with " + pings.size() + " sent messages in " + (endTime - startTime) + " ms");
+			println("Total messages received:" + ping.msgCount + " + " + pong.msgCount);
+		} catch (Exception e) {
+			Assert.fail("exception during ping-pong");
+		}
+	}
 
-  private class TimeoutActor extends Actor{
-    private long time;
-    public TimeoutActor(long time) {
-      this.time = time;
-    }
-    @Override
-    public Object receive(@Nullable Object m) {
-      long observedInterval = new Date().getTime() - this.time;
-      println("<---- TimeoutActor: Tested/observed interval: "+observedInterval + " - " + m);
-      Assert.assertTrue("This actor executed the message AFTER 100 ms", observedInterval >= 100 );
-      this.time = new Date().getTime();
-      ((Message)m).getA().after(100, new Message(this));
-      return null;
-    }
-  }
+	private class TimeoutActor extends Actor {
+		private long time;
 
-  @Test(expected=AssertionError.class)
-  public void ShouldCaptureExceptionsFromAfter_Test(){
-    TimeoutActor p1 = new TimeoutActor(new Date().getTime());
-    TimeoutActor p2 = new TimeoutActor(new Date().getTime());
-    ListenableFuture<?> future = p1.after(0, new Message(p2)); // in TimeoutActor we assert that the interval must be longer than 100 ms...
-    try {
-      future.get();
-    } catch (Exception e) {
-      Assert.fail("exception during realization of future");
-      e.printStackTrace();
-    }
-  }
+		public TimeoutActor(long time) {
+			this.time = time;
+		}
 
-  @Test
-  public void ShouldSupportAfterTest(){
-    TimeoutActor p1 = new TimeoutActor(new Date().getTime());
-    TimeoutActor p2 = new TimeoutActor(new Date().getTime());
-    ListenableFuture<?> future = p1.after(100, new Message(p2));
-    try {
-      future.get();
-    } catch (Exception e) {
-      e.printStackTrace();
-      Assert.fail("exception during realization of future");
-    }
-  }
+		@Override
+		public Object receive(@Nullable Object m) {
+			long observedInterval = new Date().getTime() - this.time;
+			println("<---- TimeoutActor: Tested/observed interval: " + observedInterval + " - " + m);
+			Assert.assertTrue("This actor executed the message AFTER 100 ms", observedInterval >= 100);
+			this.time = new Date().getTime();
+			((Message) m).getA().after(100, new Message(this));
+			return null;
+		}
+	}
+
+	@Test(expected = AssertionError.class)
+	public void ShouldCaptureExceptionsFromAfter_Test() {
+		TimeoutActor p1 = new TimeoutActor(new Date().getTime());
+		TimeoutActor p2 = new TimeoutActor(new Date().getTime());
+		ListenableFuture<?> future = p1.after(0, new Message(p2)); // in TimeoutActor we assert that the interval must be longer than 100 ms...
+		try {
+			future.get();
+		} catch (Exception e) {
+			Assert.fail("exception during realization of future");
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void ShouldSupportAfterTest() {
+		TimeoutActor p1 = new TimeoutActor(new Date().getTime());
+		TimeoutActor p2 = new TimeoutActor(new Date().getTime());
+		ListenableFuture<?> future = p1.after(100, new Message(p2));
+		try {
+			future.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail("exception during realization of future");
+		}
+	}
 }
